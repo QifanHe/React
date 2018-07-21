a6814fe66a8a180b50756193e1b92aa744e3ae71
QifanHe <heqifan0109@gmail.com>
2018-07-20 23:19:26 -0400

    trim project folder

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// all in js
+// import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// pwa: progressive web application
+// https协议的服务器上 即便断网，第二次登陆还是能看到。
+// registerServiceWorker 帮助缓存到浏览器里。
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
