3564982f7b02d78705215105a28a52f2de068969
Qifan He <heqf@bu.edu>
2018-07-23 14:11:25 -0400

    transition group added

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // all in js
 // import './index.css';
-import TodoList from './TodoList';
+import App from './App';
 
 // pwa: progressive web application
 // https协议的服务器上 即便断网，第二次登陆还是能看到。
 // registerServiceWorker 帮助缓存到浏览器里。
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 // registerServiceWorker();
