13a3c7fcdf51101ec619afabd01fe8de85020465
Qifan He <heqf@bu.edu>
2018-07-23 15:17:00 -0400

    redux hello world - half way

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,105 +1,49 @@
-import React, { Component, Fragment } from 'react';
-import './style.css'
-import TodoItem from './TodoItem'
-import axios from 'axios';
+import React, { Component, Fragment } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+import store from './store'
 
-class TodoList extends Component {
+
+class Todolist extends Component {
 
   constructor(props) {
     super(props);
-    // when a component's state or props is changed
-    // render() function will be called
-    this.state = {
-      inputValue: '',
-      list: []
-    };
+    this.state = store.getState();
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
-
-
-
 
   render() {
     return (
       <Fragment>
-        {/* asdfasdf  */}
-        <div>
-          <label htmlFor="insertArea">
-            enter content:
-          </label>
-          <input
-            className='input'
-            id="insertArea"
-            value={this.state.inputValue}
+        <div style={{margin: '10px 10px'}}>
+          <Input
             onChange={this.handleInputChange}
-        />
-         <button onClick={this.handleButtonClick}>submit</button></div>
-        <ul>
-          { this.getTodoItem() }
-        </ul>
+            value={this.state.inputValue}
+            placeholder='todo info'
+            style={{width: '300px', marginRight: '10px'}}
+          />
+        <Button type='primary'>Submit</Button>
+        </div>
+        <div>
+          <List
+            style={{marginTop:'10px', width:'300px'}}
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+          />
+        </div>
+
       </Fragment>
     )
   }
 
-  componentDidMount() {
-    axios.get('/api/todolist')
-      .then((res)=>{
-        console.log(res.data);
-        this.setState(() => {
-          return {
-            list:[...res.data]
-          }
-        })
-      })
-      .catch(()=>{alert('err')})
-  }
-
   handleInputChange(e) {
-    const value = e.target.value;
-    this.setState(() => ({
-      inputValue: value
-    }))
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
   }
 
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <Fragment key={index}>
-          <TodoItem
-            content={item}
-            index={index}
-            deleteItem={this.handleItemDelete}
-          />
-          {/* <li
-            key={index}
-            onClick={this.handleItemDelete.bind(this, index)}
-            dangerouslySetInnerHTML={{__html:item}}
-            >
-          </li> */}
-
-
-        </Fragment>
-    )
-    })
-  }
-
-
-  handleButtonClick(e) {
-    this.setState((prevState)=> ({
-      list: [...prevState.list, prevState.inputValue],
-      inputValue: ''
-    }))
-  }
-
-  handleItemDelete(index) {
-    this.setState(prevState => {
-      const list = [...prevState.list];
-      list.splice(index, 1);
-      return {list};
-    })
-  }
 }
 
-export default TodoList;
+export default Todolist;
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // all in js
 // import './index.css';
-import App from './App';
+import Todolist from './Todolist';
 
 // pwa: progressive web application
 // https协议的服务器上 即便断网，第二次登陆还是能看到。
 // registerServiceWorker 帮助缓存到浏览器里。
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Todolist />, document.getElementById('root'));
 
 
 // registerServiceWorker();
