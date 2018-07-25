932b6015ff62007b97267a3fe8c550d1fbb2bfe4
Qifan He <heqf@bu.edu>
2018-07-25 09:01:04 -0400

    mac upload 7-25

--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -5,16 +5,13 @@ const defaultState = {
 
 // reducer can receive state but cannot modify state
 export default (state = defaultState, action)=>{
+  const newState = JSON.parse(JSON.stringify(state));
   if (action.type === 'change_input_value') {
-    const newState = JSON.parse(JSON.stringify(state));
     newState.inputValue = action.value;
-    return newState;
   }
   if (action.type === 'add_todo_item') {
-    const newState = JSON.parse(JSON.stringify(state));
     newState.list.push(newState.inputValue);
     newState.inputValue = '';
-    return newState;
   }
-  return state;
+  return newState;
 }
